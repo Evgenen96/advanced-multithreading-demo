685cce0d5a25d220b11e519555c5fe89c8777b39
Evgenen96 <NikonovSE96@gmail.com>
2021-03-30 13:28:44 +0400

    sample1 add some benchmark

--- src/main/java/com/epam/sample1/ForkJoinSample.java
+++ src/main/java/com/epam/sample1/ForkJoinSample.java
@@ -5,11 +5,20 @@ import java.util.concurrent.ForkJoinPool;
 public class ForkJoinSample {
     public static final Integer FIB_INDEX = 5;
 
-    public static void main(String[] args) {
-        final ForkJoinPool pool = new ForkJoinPool();
+    public static void main(String[] args) throws InterruptedException {
+        final ForkJoinPool pool = new ForkJoinPool(20);
         FibonacciTask task = new FibonacciTask(FIB_INDEX);
-
-        Integer fibNumber = (Integer) pool.invoke(task);
-        System.out.println(fibNumber);
+        
+        pool.execute(task);
+        do {
+        	System.out.println("Parallesim " + pool.getParallelism());
+        	System.out.println("Pool size " + pool.getPoolSize());
+        	System.out.println("# tasks in queue " + pool.getQueuedTaskCount());
+        	Thread.sleep(1000);
+        } while (!task.isDone());
+        
+        System.out.println("# of tasks is" + FibonacciTask.getTaskCounter());
+        //Integer fibNumber = (Integer) pool.invoke(task);
+        //System.out.println(fibNumber);
     }
 }
