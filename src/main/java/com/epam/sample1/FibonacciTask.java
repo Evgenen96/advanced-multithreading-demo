f05012165fd331fbf4cc34da7c8884d41635e894
Evgenen96 <NikonovSE96@gmail.com>
2021-04-01 15:38:03 +0400

    sample1 optimization

--- src/main/java/com/epam/sample1/FibonacciTask.java
+++ src/main/java/com/epam/sample1/FibonacciTask.java
@@ -1,12 +1,24 @@
 package main.java.com.epam.sample1;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.RecursiveTask;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class FibonacciTask extends RecursiveTask {
     private Integer fibIndex;
-    private static AtomicInteger taskCounter = new AtomicInteger(0);
+    private static AtomicInteger taskCounter = new AtomicInteger();
+    
+    private Map<Integer, Integer> fibMap = new HashMap<>();
 
+    //treshhold
+    {
+    	fibMap.put(0, 0);
+    	fibMap.put(1, 1);
+    	fibMap.put(2, 1);
+    	fibMap.put(3, 2);
+    	fibMap.put(4, 3);
+    }
     public FibonacciTask(Integer fibIndex) {
         this.fibIndex = fibIndex;
     }
@@ -17,21 +29,20 @@ public class FibonacciTask extends RecursiveTask {
 
     @Override
     protected Integer compute() {
-        if (fibIndex == 0) {
-            return 0;
-        }
-        if (fibIndex == 1) {
-            return 1;
+        if(fibMap.containsKey(fibIndex)) {
+        	return fibMap.get(fibIndex);
         }
         final FibonacciTask task1 = new FibonacciTask(fibIndex - 1);
         task1.fork();
         taskCounter.getAndIncrement();
+        
+        final FibonacciTask task2 = new FibonacciTask(fibIndex - 2);
+        Integer result2 = task2.compute();
+        taskCounter.getAndIncrement();
+        
         Integer result1 = (Integer) task1.join();
 
-        final FibonacciTask task2 = new FibonacciTask(fibIndex - 2);
-        task2.fork();
-        taskCounter.getAndIncrement();
-        Integer result2 = (Integer) task2.join();
+        
 
         int result = result1 + result2;
         return result;
--- src/main/java/com/epam/sample1/ForkJoinSample.java
+++ src/main/java/com/epam/sample1/ForkJoinSample.java
@@ -3,22 +3,23 @@ package main.java.com.epam.sample1;
 import java.util.concurrent.ForkJoinPool;
 
 public class ForkJoinSample {
-    public static final Integer FIB_INDEX = 5;
+    public static final Integer FIB_INDEX = 20;
 
     public static void main(String[] args) throws InterruptedException {
         final ForkJoinPool pool = new ForkJoinPool(20);
         FibonacciTask task = new FibonacciTask(FIB_INDEX);
         
-        pool.execute(task);
-        do {
-        	System.out.println("Parallesim " + pool.getParallelism());
-        	System.out.println("Pool size " + pool.getPoolSize());
-        	System.out.println("# tasks in queue " + pool.getQueuedTaskCount());
-        	Thread.sleep(1000);
-        } while (!task.isDone());
+//        pool.execute(task);
+//        do {
+//        	System.out.println("Parallesim " + pool.getParallelism());
+//        	System.out.println("Pool size " + pool.getPoolSize());
+//        	System.out.println("# tasks in queue " + pool.getQueuedTaskCount());
+//        	Thread.sleep(1000);
+//        } while (!task.isDone());
         
-        System.out.println("# of tasks is" + FibonacciTask.getTaskCounter());
-        //Integer fibNumber = (Integer) pool.invoke(task);
-        //System.out.println(fibNumber);
+       
+        Integer fibNumber = (Integer) pool.invoke(task);
+        System.out.println("# of tasks is " + FibonacciTask.getTaskCounter());
+        System.out.println(fibNumber);
     }
 }
